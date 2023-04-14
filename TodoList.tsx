38a02017940c72407e2726681935f56497ee2160
jedaydiya <joloanjed@gmail.com>
2023-04-14 14:43:50 +0800

    Added Offline-first feature

--- TodoList.tsx
+++ TodoList.tsx
@@ -1,37 +1,64 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { supabase } from "./supabase";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import "react-native-url-polyfill/auto";
 
+// From Supabase
 type Item = {
   id: number;
   description: string;
   completed: boolean;
 };
 
+// For rendering to local Storage
+interface Todo {
+  id: number;
+  description: string;
+  completed: boolean;
+}
+
 const TodoList: React.FC = () => {
   const [items, setItems] = useState<Item[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([]);
 
+  // This function fetches the data from supabase then saves it to async storage
   useEffect(() => {
-    const getItems = async () => {
+    const fetchItems = async () => {
       const { data, error } = await supabase.from("Items").select("*");
       if (error) {
         console.log(error);
-      } else {
-        setItems(data as Item[]);
-        console.log(items);
+        return;
+      }
+      setItems(data as Item[]);
+      await AsyncStorage.setItem("items", JSON.stringify(data));
+    };
+    fetchItems();
+  }, []);
+
+  // This functions gets the data from asyncStorage
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const storedData = await AsyncStorage.getItem("items");
+        if (storedData !== null) {
+          setTodos(JSON.parse(storedData));
+        }
+      } catch (error) {
+        console.log(error);
       }
     };
-    getItems();
+
+    getData();
   }, []);
 
   return (
     <View style={styles.container}>
       <Text>Hello</Text>
-      {items.map((item) => (
-        <View key={item.id}>
-          <Text>{item.description}</Text>
-          <Text>{item.completed}</Text>
+      {todos.map((todo) => (
+        <View key={todo.id}>
+          <Text>{todo.description}</Text>
         </View>
       ))}
     </View>
