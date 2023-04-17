403f0810c0d530a80760ad10e2639b50f5dd9b83
jedaydiya <joloanjed@gmail.com>
2023-04-17 20:09:53 +0800

    Added Nativewind

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { Card } from "@rneui/base";
-import { CardTitle } from "@rneui/base/dist/Card/Card.Title";
+
 type Props = {};
 
 const HomeScreen = (props: Props) => {
   return (
     <>
       <View>
-        <Card>
-          <Card.Title>Quiz App</Card.Title>
-        </Card>
+        <Text>Home</Text>
       </View>
     </>
   );
